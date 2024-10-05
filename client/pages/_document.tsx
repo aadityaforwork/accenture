f429d72b67ca9ff43ae519ab0b1403708fe7fff5
aadityaforwork <aadityamalani15@gmail.com>
2024-10-05 12:39:45 +0530

    type error fixxed

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -27,7 +27,7 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin="true"
+          // crossOrigin="true"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap"
